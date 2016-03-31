b79a3362ba7d7c701753322a2a428fb9ad30151e
Bernardo <bernardo.belchior1@gmail.com>
2016-03-31 18:12:47 +0100

    Program lists files with forks properly.

--- proj1/lsdir.c
+++ proj1/lsdir.c
@@ -1,4 +1,5 @@
 #include<dirent.h>
+#include<sys/wait.h>
 #include<errno.h>
 #include<sys/types.h>
 #include<fcntl.h>
@@ -13,9 +14,6 @@ typedef struct {
 	char* name;
 	char* path;
 } file_path; 
-
-file_path* (*files);
-int file_no;
 
 void print_file(const char* path, int output) {
 	struct stat file_info;
@@ -74,57 +72,60 @@ void print_directory(const char* name, int output) {
 
 }
 
-void read_directory(int pipe, const char* dir_path) {
+void read_directory(int pipe_fd, const char* dir_path) {
 	DIR* directory;
 	struct dirent* child;		
 
 	if((directory = opendir(dir_path)) == NULL) {
-		perror(strerror(errno));
-		perror("The directory ");
-		perror(dir_path);
-		perror(" could not be open.\n");
+		fprintf(stderr, "The directory %s could not be opened.\n", dir_path);
 	}
 
 	while((child = readdir(directory)) != NULL) {
-		int is_dir = is_dirent_a_directory(dir_path);
-		if(is_dir == 1) {
-			//fork
-			int pid, fd[2];
-			char path[strlen(dir_path)+strlen(child->d_name)+1];
-			strcpy(path, dir_path);
+		char path[strlen(dir_path)+strlen(child->d_name)+2];
+		strcpy(path, dir_path);
+		strcat(path, child->d_name);
 
-			if(pipe(fd) < 0) {
-				perror("Error creating pipe.\n");
-			}
-
-			if((pid = fork()) == -1) {
-				//in case of error
-				perror(strerror(errno));
-				perror("Error creating a child.\n");
-			}	else if (pid >  0) {
-				//father
-				close(fd[1]); //close the sender
-				if((waitpid(pid, NULL, 0)) < 0) {
-					perror("waitpid error.\n");
+		int is_dir = is_dirent_a_directory(path);
+		if(strcmp(child->d_name, ".") && strcmp(child->d_name, "..")) {
+			if(is_dir == 1) {
+				//fork
+				int pid, fd[2];
+		
+				if(pipe(fd) < 0) {
+					perror("Error creating pipe.\n");
 				}
-				char temp[200]; //dynamic allocate memory afterwards
-				//read path
-				read(fd[0], temp, 200);
-				write(pipe, temp, 200);
-				//read name
-				read(fd[0], temp, 200);
-				write(pipe, temp, 200);
-				//TODO: i was her
-			} else {
-				//son
-				strcat(path, child->d_name);
-				close(fd[0]); //close the receiver
-				read_directory(path, fd[1]);	
+
+				if((pid = fork()) == -1) {
+					//in case of error
+					perror(strerror(errno));
+					perror("Error creating a child.\n");
+				}	else if (pid >  0) {
+					//father
+					close(fd[1]); //close the sender
+					if((waitpid(pid, NULL, 0)) < 0) {
+						perror("waitpid error.\n");
+					}
+					char temp[200]; //dynamic allocate memory afterwards
+					//read path
+					read(fd[0], temp, 200);
+					write(pipe_fd, temp, 200);
+					//read name
+					read(fd[0], temp, 200);
+					write(pipe_fd, temp, 200);
+				} else {
+					//son
+					char terminator[] = "/\0";
+					strcat(path, terminator);
+					close(fd[0]); //close the receiver
+					read_directory(fd[1], path);	
+					exit(0);
+				}
+			} else if(is_dir == 0) {
+				//send to father.
+//				write(STDOUT_FILENO, dir_path, strlen(dir_path) + 1);
+				write(STDOUT_FILENO, child->d_name, strlen(child->d_name) + 1);
+				write(STDOUT_FILENO, "\n", 1);
 			}
-		} else if(is_dir == 0) {
-			//send to father.
-			write(pipe, path, strlen(path) + 1);
-			write(pipe, name, strlen(name) + 1);
 		}
 	}
 }
@@ -134,7 +135,7 @@ int main(int argc, char* argv[]){
 	int file = open(filepath, O_WRONLY | O_TRUNC | O_CREAT);
 	int pid;
 	int fd[2];
-	
+
 	if(file == -1) {
 		perror(strerror(errno));
 		perror("Redirecting to console...\n");
@@ -144,25 +145,36 @@ int main(int argc, char* argv[]){
 	char *header = "Serial number | File size | Modification Date | File name\n";
 	write(file, header, strlen(header));
 
-	files = (file_path**) malloc(10*sizeof(file_path*));
-	file_no = 0;
-
 	if(pipe(fd) < 0) {
 		perror("Error creating main pipe.\n");
 		exit(1);
 	}
-		
-	
+
+
 	if((pid = fork()) < 0) {
 		perror("Error creating main fork.\n");
 	} else if (pid > 0) {
 		//father
+		int no_files = 0;
+		int files_size = 10;
+		file_path (*files) = (file_path*) malloc(files_size*sizeof(file_path));
+
 		//do the sorting and whatever.
 		close(fd[1]);			
+		if(waitpid(pid, NULL, 0) < 0) {
+			perror("Error waiting for main child.\n");
+		}
+
+		//read from the queue
+		char temp[200];
+		read(fd[0], &temp, 199);
+		temp[199] = '\0';
+		printf("%s", temp);		
+
 	} else {
 		//son
 		close(fd[0]);
-		read_directory(argv[1], fd[1]);
+		read_directory(fd[1], argv[1]);
 	}
 	return 0;
 }
