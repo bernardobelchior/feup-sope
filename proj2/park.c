49bb55b2d55a172577c4867cc75efb948a3f3c4a
Edgar Passos <epassos26@gmail.com>
2016-05-12 12:38:48 +0100

    Initial commit

--- proj2/park.c
+++ proj2/park.c
@@ -1,3 +1,9 @@
-int main(int argc, char* argv[]) {
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]){
 
 }
