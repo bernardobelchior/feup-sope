ffd364e1d2740794ea116ee7cc388d992a7d2f57
bernardobelchior1 <bernardo.belchior1@gmail.com>
2016-05-12 12:38:51 +0100

    Created main for generator and udpated makefile.

--- proj2/generator.c
+++ proj2/generator.c
@@ -1,4 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef enum { NORTH, SOUTH, EAST, WEST } direction;
+
 int main(int argc, char* argv[]) {
+	
+	if(argc != 3) {
+		fprintf(stderr, "Incorrect use of program.\nShould be used in this format:\ngerador <generation_time> <update_rate>\n");
+		return 1;
+	}
+
+	int generation_time = atoi(argv[1]);
+	int update_rate = atoi(argv[2]);
 
 	return 0;
 }
