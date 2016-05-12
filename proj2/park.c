79a4d3f20ed2bad448e70eee49221b561f498185
Edgar Passos <epassos26@gmail.com>
2016-05-12 20:13:48 +0100

    Created the prototype for the controller function

--- proj2/park.c
+++ proj2/park.c
@@ -2,17 +2,20 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <unistd.h>
 
-#define NORTH 0
-#define EAST  1
-#define SOUTH 2
-#define WEST  3
+#define NORTH 0 //     0
+#define WEST  1 //  1     3
+#define SOUTH 2 //     2
+#define EAST  3
 
 void print_usage();
+void *controller_func (void *arg);
 
 int main(int argc, char *argv[]){
 	
+	//Validating input
 	if(argc != 3){
 		print_usage();
 		return 1;
@@ -28,20 +31,26 @@ int main(int argc, char *argv[]){
 		return 1;
 	}
 
+	//creating the 4 "controller" threads
 	pthread_t controllers[4];
 
 	int i;
 
 	for(i = 0; i < 4; i++){
-		if(pthread_create(&controllers[i],NULL,NULL,NULL) != 0){
+		if(pthread_create(&controllers[i],NULL,controller_func,NULL) != 0){ //TODO create thread functions
 			printf("\tmain() :: error creating controller threads\n");
 			return 3;
 		}
 	}
+
 	return 0;
 }
 
 void print_usage(){
 
 	printf("Run as: ./parque <N_LUGARES> <TEMPO_ABERTURA>\nN_LUGARES and TEMPO_ABERTURA must be positive integers\n");
+}
+
+void *controller_func(void *arg){
+
 }
