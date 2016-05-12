ec5ce34eb2838dabb3d95c76a7154f99205f871a
Edgar Passos <epassos26@gmail.com>
2016-05-12 20:00:56 +0100

    Parsing input and creating controller threads

--- proj2/park.c
+++ proj2/park.c
@@ -4,6 +4,44 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[]){
+#define NORTH 0
+#define EAST  1
+#define SOUTH 2
+#define WEST  3
 
+void print_usage();
+
+int main(int argc, char *argv[]){
+	
+	if(argc != 3){
+		print_usage();
+		return 1;
+	}
+
+	int n_spaces, time_open;
+
+	n_spaces = atoi(argv[1]);
+	time_open = atoi(argv[2]);
+
+	if(n_spaces < 0 || time_open < 0){
+		print_usage();
+		return 1;
+	}
+
+	pthread_t controllers[4];
+
+	int i;
+
+	for(i = 0; i < 4; i++){
+		if(pthread_create(&controllers[i],NULL,NULL,NULL) != 0){
+			printf("\tmain() :: error creating controller threads\n");
+			return 3;
+		}
+	}
+	return 0;
+}
+
+void print_usage(){
+
+	printf("Run as: ./parque <N_LUGARES> <TEMPO_ABERTURA>\nN_LUGARES and TEMPO_ABERTURA must be positive integers\n");
 }
