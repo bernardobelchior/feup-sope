b215c77d74960f9c01e49746f76c0e18c46b3933
bernardobelchior1 <bernardo.belchior1@gmail.com>
2016-05-12 12:57:44 +0100

    Created start_generator function.

--- proj2/generator.c
+++ proj2/generator.c
@@ -1,17 +1,29 @@
 #include <stdio.h>
+#include <pthread.h>
 #include <stdlib.h>
 
 typedef enum { NORTH, SOUTH, EAST, WEST } direction;
 
-int main(int argc, char* argv[]) {
+
+
+void start_generator(int update_rate) {
 	
+}
+
+
+int main(int argc, char* argv[]) {
 	if(argc != 3) {
 		fprintf(stderr, "Incorrect use of program.\nShould be used in this format:\ngerador <generation_time> <update_rate>\n");
 		return 1;
 	}
 
+	srand((unsigned int) time(NULL));
+
 	int generation_time = atoi(argv[1]);
 	int update_rate = atoi(argv[2]);
+
+
+	start_generator(update_rate);
 
 	return 0;
 }
