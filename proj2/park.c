22b9be942b4abbeed897d969486638c97cf72932
Edgar Passos <epassos26@gmail.com>
2016-05-12 20:54:53 +0100

    Passing thread arguments correctly

--- proj2/park.c
+++ proj2/park.c
@@ -16,6 +16,8 @@
 #define SOUTH 2 //     2
 #define EAST  3
 
+#define NUM_CONTROLLERS 4
+
 void print_usage();
 void *controller_func (void *arg);
 
@@ -49,11 +51,20 @@ int main(int argc, char *argv[]){
 	pthread_t controllers[4];
 
 	int i;
+	int thrargs[NUM_CONTROLLERS];
 
-	for(i = 0; i < 4; i++){
-		if(pthread_create(&controllers[i],NULL,controller_func,NULL) != 0){ 
+	for(i = 0; i < NUM_CONTROLLERS; i++){
+		thrargs[i] = i;
+		if(pthread_create(&controllers[i],NULL,controller_func, &thrargs[i]) != 0){ 
 			printf("\tmain() :: error creating controller threads\n");
 			return 3;
+		}
+	}
+	
+	for(i = 0; i < 4; i++){
+		if(pthread_join(controllers[i],NULL) != 0){ 
+			printf("\tmain() :: error joining controller threads\n");
+			return 4;
 		}
 	}
 
@@ -80,4 +91,29 @@ void print_usage(){
 void *controller_func(void *arg){
 
 	//TODO create FIFOS, read requests, create valet threads and react to the closure of the park
+	
+
+	int side = (*(int *) arg);
+
+	switch (side){
+		case NORTH:
+			printf("North Side yo!\n");
+			break;
+			
+		case WEST:
+			printf("West Side yo!\n");
+			break;
+
+		case SOUTH:
+			printf("South Side yo\n");
+			break;
+
+		case EAST:
+			printf("East Side yo\n");
+			break;
+
+		default:
+			break; //not expected
+	}
+	return NULL;
 }
