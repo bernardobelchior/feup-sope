bdad65048bd1a2ab6aa8bb142636143b4949018f
Edgar Passos <epassos26@gmail.com>
2016-05-12 20:22:45 +0100

    Documenting some code

--- proj2/park.c
+++ proj2/park.c
@@ -1,3 +1,9 @@
+/**
+ * park.c
+ *
+ * Created by Bernardo Belchior and Edgar Passos
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
@@ -13,6 +19,14 @@
 void print_usage();
 void *controller_func (void *arg);
 
+/**
+ * main thread:
+ * 
+ * should initialize the needed global variables, 
+ * create 4 controller threads and wait for their termination,
+ * calculate and save global statistics
+ *
+ */
 int main(int argc, char *argv[]){
 	
 	//Validating input
@@ -37,7 +51,7 @@ int main(int argc, char *argv[]){
 	int i;
 
 	for(i = 0; i < 4; i++){
-		if(pthread_create(&controllers[i],NULL,controller_func,NULL) != 0){ //TODO create thread functions
+		if(pthread_create(&controllers[i],NULL,controller_func,NULL) != 0){ 
 			printf("\tmain() :: error creating controller threads\n");
 			return 3;
 		}
@@ -46,11 +60,24 @@ int main(int argc, char *argv[]){
 	return 0;
 }
 
+/**
+ * text to be printed when the program is used incorrectly
+ * 
+ */
 void print_usage(){
 
 	printf("Run as: ./parque <N_LUGARES> <TEMPO_ABERTURA>\nN_LUGARES and TEMPO_ABERTURA must be positive integers\n");
 }
 
+/**
+ * controller thread:
+ *
+ * should create a FIFO,
+ * handle its requests (entrance, time for parking, vehicle identifier, name of private FIFO for vehicle thread)
+ * create an employee thread to handle the request and pass it its info
+ * wait for closure of the park to attend any remaining requests and close the FIFO
+ */
 void *controller_func(void *arg){
 
+	//TODO create FIFOS, read requests, create valet threads and react to the closure of the park
 }
